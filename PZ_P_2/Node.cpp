f5c98a2d3dcdc971b3f0894ef6e590c548c48c3f
Marta <martaprzyszowa@gmail.com>
2024-10-27 11:17:10 +0100

    usuniecie podwojonych

--- PZ_P_2/Node.cpp
+++ PZ_P_2/Node.cpp
@@ -1,31 +1,3 @@
 #pragma once
 #include "Node.h"
-int Node::ret_data()
-{
-	return data;
-}
-
-void Node::set_data(int x)
-{
-	data = x;
-}
-
-Node* Node::ret_lewy()
-{
-	return lewy;
-}
-
-Node* Node::ret_prawy()
-{
-	return prawy;
-}
-
-void Node::set_lewy(Node* x)
-{
-	lewy = x;
-}
-void Node::set_prawy(Node* x)
-{
-	prawy = x;
-}
 
